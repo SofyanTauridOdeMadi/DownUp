28d46a63c2890742111a6e7ee989d22efd06059d
SOFYAN TAURID ODE MADI <sofyantauridodemadi@gmail.com>
2025-01-04 23:46:12 +0800

    Siap...

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DownUp App',
+      title: 'DownUp',
       theme: ThemeData(
         // This is the theme of your application.
         //
